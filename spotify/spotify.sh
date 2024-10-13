24b3340bbfe86c04b7dd45eab0693340b4959b90
Kylie C <kyliepc@proton.me>
2024-10-13 16:05:49 -0400

    spotify: play URI in existing instances
    also dont stop playing if no uri is passed & reopened

--- spotify/spotify.sh
+++ spotify/spotify.sh
@@ -9,5 +9,17 @@ if [[ -f "${XDG_CONFIG_HOME}/spotify-flags.conf" ]]; then
     echo "User flags:" "${SPOTIFY_USER_FLAGS[@]}"
 fi
 
-# Launch Spotify with the given flags
-exec /opt/spotify/spotify "${SPOTIFY_USER_FLAGS[@]}" "$@"
+# Spotify supports opening in existing instance but it needs a specific format
+convert_uri() {
+    local output_uri="${1/:\/\//:}"
+    local output_uri="${output_uri/\//:}"
+    local output_uri=$(echo "$output_uri" | sed 's/\?.*si=/\:/')
+    echo "$output_uri"
+}
+
+execute='/opt/spotify/spotify "${SPOTIFY_USER_FLAGS[@]}"'
+if [[ "$1" == spotify:* ]]; then
+    eval $execute --uri=$(convert_uri "$1")
+else
+    eval $execute
+fi
